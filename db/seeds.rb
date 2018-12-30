bf29baaa5218e01dae8f0b9b7a208262f5196101
marycodes2 <mbeliveau92@gmail.com>
2018-12-29 23:17:12 -0500

    started improving card content

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,25 +7,25 @@
 mary = User.new(name:"Mary", access_type:"user")
 mary.save
 
-bulbs = Card.new(stars:1, dollar_savings:4, upfront_cost:1, score:20, name: "Energy Efficienct Light Bulb", description:"Replace incandescent light bulbs in your house with efficient, LED bulbs.", image:"ledBulb")
+bulbs = Card.new(stars:1, dollar_savings:4, upfront_cost:1, score:20, name: "Buy Energy Efficienct Light Bulbs", description:"Replacing incandescent light bulbs in your house with efficient, LED bulbs is an easy, inexpensive way to save energy. Plus, wave constantly replacing bulbs goodbye: the average LED life expectancy is 14 years!", image:"ledBulb")
 bulbs.save
 
-windows = Card.new(stars:4, dollar_savings:325, upfront_cost:3, score:500, name: "Energy Efficient Windows", description:"Replace single pane windows in your home with energy efficient windows", image:"windows")
+windows = Card.new(stars:4, dollar_savings:325, upfront_cost:3, score:500, name: "Energy Efficient Windows", description:"Keep your home warm and efficient by replacing old windows with their energy efficient counterparts. Replacing one window could save you $350 annually!", image:"windows")
 windows.save
 
-fridge = Card.new(stars:1, dollar_savings:38, upfront_cost:2, score:200, name: "Energy Efficient Refrigerator", description:"Replace your old refrigerator with an energy efficient refrigerator", image:"fridge")
+fridge = Card.new(stars:1, dollar_savings:38, upfront_cost:2, score:200, name: "Energy Efficient Refrigerator", description:"Flip your old, energy guzzling refrigerator for a newer, efficient model and enjoy a more efficient product that could save you $270 over the next five years!", image:"fridge")
 fridge.save
 
-car = Card.new(stars:2, dollar_savings:10, upfront_cost:0, score:30, name: "Drive Less", description:"Reduce the number of miles you drive your car by over 10 miles this week", image:"biking")
+car = Card.new(stars:2, dollar_savings:10, upfront_cost:0, score:30, name: "Drive Less", description:"Help the environment and your six-pack this season by biking to work instead of driving. Burn 750 calories and 0 fossil fuels before coffee with this mood-boosting commute.", image:"biking")
 car.save
 
-computer = Card.new(stars:1, dollar_savings:13, upfront_cost:0, score:20, name: "Enable Sleep Mode", description:"Enable sleep mode on your computer and/or monitor", image:"computer")
+computer = Card.new(stars:1, dollar_savings:13, upfront_cost:0, score:20, name: "Enable Sleep Mode", description:"A one-time, two-second way to save $13 dollars a year: enable sleep mode on your computer and/or monitor for instant savings.", image:"computer")
 computer.save
 
-trash = Card.new(stars:3, dollar_savings:0, upfront_cost:0, score:120, name: "No Trash for Three Days", description:"Refrain from creating any trash for three days!", image:"no_trash")
+trash = Card.new(stars:3, dollar_savings:0, upfront_cost:0, score:120, name:"No Trash for Three Days", description: "Challenge yourself to not producing any trash for three days. This spiritual experience will help you reflect on your consumption habbits and Mother Earth will thank you!", image:"no_trash")
 trash.save
 
-winter_heat = Card.new(stars:2, dollar_savings:3, upfront_cost:0, score:40, name: "Turn Down Thermostat", description:"Turn down your thermostat during the winter months.", image:"thermostat_down")
+winter_heat = Card.new(stars:2, dollar_savings:3, upfront_cost:0, score:40, name: "Turn Down Your Thermostat", description:"Cuddling up with an extra sweater this winter can save you hundreds of dollars and reduce strain on the electricity grid.", image:"cuddle_fire")
 winter_heat.save
 
 clean_power = Card.new(stars:5, dollar_savings:0, upfront_cost:2, score:250, name: "Purchase Clean Power", description:"Purchase clean energy from your utility or a third-party distributor.", image:"green_power")
