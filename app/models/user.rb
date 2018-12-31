77d95cc79b890a321f531ce862df3b3035fdd016
marycodes2 <mbeliveau92@gmail.com>
2018-12-30 20:15:51 -0500

    fixed login bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, uniqueness: { case_sensitive: false }
   validates :username, presence: true
-  validates :upgraded_bulbs, numericality: {greater_than: -1, less_than: 100}
-  validates :upgraded_windows, numericality: {greater_than: -1, less_than: 1000}
-  validates :driving_miles_saved_in_week, numericality: {greater_than: -1, less_than: 100000}
-  validates :num_sleep_monitors, numericality: {greater_than: -1, less_than: 1000}
-  validates :degrees_decreased_winter, numericality: {greater_than: -1, less_than: 100}
-  validates :degrees_increased_summer, numericality: {greater_than: -1, less_than: 100}
   has_many :user_cards
   has_many :cards, through: :user_cards
 end
