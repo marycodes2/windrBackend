a97d65118b94ec704cc3c26661bd37a377ff22aa
marycodes2 <mbeliveau92@gmail.com>
2019-01-02 17:17:02 -0500

    fixed controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ class ApplicationController < ActionController::API
 
 
   def authorized
-    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+    # render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
   end
 
 
