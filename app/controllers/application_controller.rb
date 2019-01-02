873fe4c722232ac0950cf86b3f50c231c0de277c
marycodes2 <mbeliveau92@gmail.com>
2019-01-02 17:06:55 -0500

    fixed application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  # before_action :authorized
+  before_action :authorized
 
 # purpose: guven a payload, return a token
   def encode_token(payload)
